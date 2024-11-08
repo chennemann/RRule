5a63cd8ebbc1d9993edef1b47514abc9f6a6d7e7
Christoph Hennemann <christoph.hennemann@gmail.com>
2024-11-08 01:18:06 +0100

    Extend RRule parser

--- src/commonTest/kotlin/de/chennemann/rrule/RRuleParserTest.kt
+++ src/commonTest/kotlin/de/chennemann/rrule/RRuleParserTest.kt
@@ -1,11 +1,10 @@
 package de.chennemann.rrule
 
-import de.chennemann.datetime.atStartOfDay
-import de.chennemann.datetime.now
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format.char
 import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import kotlin.test.Test
 
 
@@ -30,16 +29,14 @@ class RRuleParserTest {
             .message shouldBeEqualTo "No RRule String Representation found"
     }
 
+    @Test
+    fun unknownProperty() {
+        parse("SOME_UNKNOWN_PROPERTY") shouldBeEqualTo RRule()
+    }
 
     @Test
-    fun property() {
-        parse("SOME_PROPERTY") shouldBeEqualTo RRule()
-        parse("DTSTART") shouldBeEqualTo RRule(
-            startDateConfiguration = StartDateProperty(
-                startDateTime = LocalDate.now().atStartOfDay(TimeZone.UTC),
-                timeZone = TimeZone.UTC
-            )
-        )
+    fun dtstart() {
+        parse("DTSTART") shouldBeEqualTo RRule()
         parse("DTSTART:19970714T173000Z") shouldBeEqualTo RRule(
             startDateConfiguration = StartDateProperty(
                 startDateTime = LocalDateTime(
@@ -51,6 +48,45 @@ class RRuleParserTest {
                     second = 0
                 ),
                 timeZone = TimeZone.UTC
+            )
+        )
+        parse("DTSTART:19970714T173000") shouldBeEqualTo RRule(
+            startDateConfiguration = StartDateProperty(
+                startDateTime = LocalDateTime(
+                    year = 1997,
+                    monthNumber = 7,
+                    dayOfMonth = 14,
+                    hour = 17,
+                    minute = 30,
+                    second = 0
+                ),
+                timeZone = TimeZone.currentSystemDefault()
+            )
+        )
+        parse("DTSTART;TZID=America/New_York:19970714T173000") shouldBeEqualTo RRule(
+            startDateConfiguration = StartDateProperty(
+                startDateTime = LocalDateTime(
+                    year = 1997,
+                    monthNumber = 7,
+                    dayOfMonth = 14,
+                    hour = 17,
+                    minute = 30,
+                    second = 0
+                ),
+                timeZone = TimeZone.of("America/New_York")
+            )
+        )
+        parse("DTSTART;TZID=America/New_York:19970714T173000Z") shouldBeEqualTo RRule(
+            startDateConfiguration = StartDateProperty(
+                startDateTime = LocalDateTime(
+                    year = 1997,
+                    monthNumber = 7,
+                    dayOfMonth = 14,
+                    hour = 13,
+                    minute = 30,
+                    second = 0
+                ),
+                timeZone = TimeZone.of("America/New_York")
             )
         )
     }
@@ -145,7 +181,11 @@ val ALLOWED_CONTINUATION_WHITESPACES = setOf(" ", "\t")
 
 
 enum class PropertyType {
-    DTSTART
+    DTSTART, RRULE
+}
+
+enum class Modifier {
+    TZID, FREQ, INTERVAL, COUNT, UNTIL, BYDAY, BYMONTH
 }
 
 
@@ -159,21 +199,61 @@ fun parse(rrule: String): RRule {
         contentLines.forEach { line ->
             when {
                 line isFor PropertyType.DTSTART -> parseRecurrenceStart(line)
+                line isFor PropertyType.RRULE -> parseRRule(line)
             }
         }
     }
 }
 
+// Formatter for datetime strings without timezone information
+//val LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")
+val LOCAL_DATE_TIME_FORMATTER = LocalDateTime.Format {
+    year();monthNumber();dayOfMonth();char('T');hour();minute();second()
+}
 
 class RRuleBuilder() {
     var timeZone: TimeZone = TimeZone.UTC
     var start: LocalDateTime? = null
+    var frequency: Frequency? = null
+    var interval: Int = 1 // default to 1 if unspecified
+    var count: Int? = null
+    var until: LocalDateTime? = null
 
-    fun parseRecurrenceStart(dtstartContentline: ContentLine) {
-        
+    internal fun parseRecurrenceStart(dtstart: ContentLine) {
+        val dtstartValue = dtstart.values.propertyValue() // Strip 'Z' if present
+        if (dtstartValue != null) {
+
+            start = LocalDateTime.parse(dtstartValue.removeSuffix("Z"), LOCAL_DATE_TIME_FORMATTER)
+
+            val isUTCValue = dtstartValue.endsWith("Z")
+            val tzid = dtstart.params.paramValue(Modifier.TZID)
+            val hasTZID = tzid != null
+
+            when {
+                hasTZID && isUTCValue -> {
+                    timeZone = TimeZone.of(tzid)
+                    start = start?.toInstant(TimeZone.UTC)?.toLocalDateTime(TimeZone.of(tzid))
+                }
+
+                hasTZID && !isUTCValue -> {
+                    timeZone = TimeZone.of(tzid)
+                }
+
+                !hasTZID && isUTCValue -> {
+                    timeZone = TimeZone.UTC
+                }
+
+                // Default to system timezone or UTC if no TZID and no Z
+                else -> timeZone =TimeZone.currentSystemDefault()
+
+            }
+        }
     }
 
-    fun build(): RRule {
+    internal fun parseRRule(line: ContentLine) {
+    }
+
+    internal fun build(): RRule {
         return RRule(
             startDateConfiguration = start?.let { StartDateProperty(startDateTime = it, timeZone = timeZone) }
         )
@@ -186,11 +266,35 @@ fun rrule(init: RRuleBuilder.() -> Unit): RRule {
     return builder.build()
 }
 
+fun RRuleBuilder.frequency(frequency: Frequency) = apply { this.frequency = frequency }
+fun RRuleBuilder.interval(interval: Int) = apply { this.interval = interval }
+fun RRuleBuilder.count(count: Int) = apply { this.count = count }
+fun RRuleBuilder.until(until: LocalDateTime) = apply { this.until = until }
+fun RRuleBuilder.timezone(timeZone: TimeZone) = apply { this.timeZone = timeZone }
+
 
 data class RRule(
-    private val startDateConfiguration: StartDateProperty? = null
+    private val startDateConfiguration: StartDateProperty? = null,
+    val frequency: Frequency? = null,
+    val interval: Int = 1,
+    val count: Int? = null,
+    val until: LocalDateTime? = null
 ) {
     val startDate = startDateConfiguration?.startDateTime?.toInstant(startDateConfiguration.timeZone)
+
+    override fun toString(): String {
+        val components = mutableListOf<String>()
+
+        startDateConfiguration?.let {
+            components.add("DTSTART;TZID=${it.timeZone.id}:${it.startDateTime}")
+        }
+        frequency?.let { components.add("FREQ=${it.name}") }
+        if (interval > 1) components.add("INTERVAL=$interval")
+        count?.let { components.add("COUNT=$it") }
+        until?.let { components.add("UNTIL=$it") }
+
+        return components.joinToString(";")
+    }
 }
 
 data class StartDateProperty(
@@ -198,17 +302,47 @@ data class StartDateProperty(
     val timeZone: TimeZone = TimeZone.UTC
 )
 
+sealed interface Param {
+    val name: String
+
+    data class MarkerParam(override val name: String) : Param
+    data class ConfigParam(override val name: String, val value: String) : Param
+}
+
+fun List<Param>.paramValue(enum: Modifier) = this
+    .filterIsInstance<Param.ConfigParam>()
+    .firstOrNull { it.name == enum.name }
+    ?.value
+
+sealed interface Value {
+
+    val value: String
+
+    data class StandaloneValue(override val value: String) : Value
+    data class ConfigValue(val name: String, override val value: String) : Value
+}
+
+fun List<Value>.valueModifierValue(enum: Modifier) = this
+    .filterIsInstance<Value.ConfigValue>()
+    .firstOrNull { it.name == enum.name }
+    ?.value
+
+fun List<Value>.propertyValue() = this
+    .filterIsInstance<Value.StandaloneValue>()
+    .firstOrNull()
+    ?.value
+
+
 data class ContentLine(
     val name: String,
-    val params: List<String> = emptyList(),
-    val values: List<String> = emptyList()
+    val params: List<Param> = emptyList(),
+    val values: List<Value> = emptyList()
 ) {
 
     infix fun isFor(propertyType: PropertyType): Boolean {
         return propertyType.name.equals(name, ignoreCase = true)
     }
 }
-
 
 
 private fun String.unfold(): List<String> = this
@@ -228,13 +362,34 @@ private fun String.unfold(): List<String> = this
 private fun parseContentLine(contentLineString: String): ContentLine {
     return when {
         contentLineString.contains(PROPERTY_VALUE_DELIMITER) -> {
-            val (propertyConfiguration, propertyValue) = contentLineString.split(PROPERTY_VALUE_DELIMITER, limit = 2)
-            val values = propertyValue.split(VALUE_DELIMITER)
+            val (propertyConfiguration, propertyValue) = contentLineString.split(
+                PROPERTY_VALUE_DELIMITER,
+                limit = 2
+            )
+            @Suppress("NAME_SHADOWING") val values = propertyValue.split(VALUE_DELIMITER).map { value ->
+                when {
+                    value.contains("=") -> {
+                        val (name, value) = value.split("=", limit = 2)
+                        Value.ConfigValue(name, value)
+                    }
+
+                    else -> Value.StandaloneValue(value)
+                }
+            }
             when {
                 propertyConfiguration.contains(PARAM_DELIMITER) -> {
-                    val propertyParams = propertyConfiguration.splitToSequence(PARAM_DELIMITER)
+                    val propertyParams = propertyConfiguration.split(PARAM_DELIMITER)
                     val propertyName = propertyParams.first()
-                    val params = propertyParams.drop(1).toList()
+                    val params = propertyParams.drop(1).map { param ->
+                        when {
+                            param.contains("=") -> {
+                                val (name, value) = param.split("=", limit = 2)
+                                Param.ConfigParam(name, value)
+                            }
+
+                            else -> Param.MarkerParam(param)
+                        }
+                    }
                     ContentLine(name = propertyName, params = params, values = values)
                 }
 
@@ -245,13 +400,6 @@ private fun parseContentLine(contentLineString: String): ContentLine {
         else -> ContentLine(contentLineString)
     }
 }
-
-
-
-
-
-
-
 
 
 private fun <T> List<T>.replaceAt(index: Int, transformation: (T) -> T): List<T> {
